aee7b66defae767bcf5f9ad745ec5fe9010ce919
Noah Février Deltor <noahdeltor@gmail.com>
2024-03-12 17:10:33 -0400

    Fin Script Door

--- Assets/Scripts/SciptableObject/FutureState/FutureStateData.cs
+++ Assets/Scripts/SciptableObject/FutureState/FutureStateData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="FutureState_000",menuName ="Scriptable Objet/FutureState")]
+[CreateAssetMenu(fileName ="FutureState_000",menuName ="Scriptable Object/FutureState")]
 
 public class FutureStateData : ScriptableObject
 {
